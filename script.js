9aa78a18c599d10a77a9710af34d00650d1fbe1f
Carman Mach <mach.carman@gmail.com>
2021-03-21 21:33:15 -0400

    mixins, headers, stripping styles added - nav menu functional

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-// const TypeWriter = function(txtElement, words, wait = 3000) {
-//     this.txtElement = txtElement;
-//     this.words = words;
-//     this.txt = '';
-//     this.wordIndex = 0;
-//     this.wait = parseInt(wait, 10);
-//     this.type();
-//     this.isDeleting = false;
-// }
+const myApp = {};
 
-// // Type Method
-// TypeWriter.prototype.type = function() {
-//     const current = this.wordIndex % this.words.length;
-//     const fullTxt = this.words[current];
-    
-//     if (this.isDeleting) {
-//         this.txt = fullTxt.substring(0, this.txt.length - 1);
-//     } else {
-//         this.txt = fullTxt.substring(0, this.txt.length + 1);
-//     }
-
-//     this.txtElement.innerHTML = `<span class="text">${this.txt}</span>`;
-
-//     let typeSpeed = 300;
-
-//     if (this.isDeleting) {
-//         typeSpeed /= 2;
-//     } 
-
-//     if (!this.isDeleting && this.txt === fullTxt) {
-//         typeSpeed = this.wait;
-//         this.isDeleting = true;
-//     } else if (this.isDeleting && this.txt === '') {
-//         this.isDeleting = false;
-//         this.wordIndex++;
-//         typeSpeed = 500;
-//     }
-    
-//     setTimeout(() => this.type(), typeSpeed);
-// }
-
-
-
+myApp.toggleMenu = function () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const navMenu = document.querySelector('.nav-container');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+        if (!menuOpen) {
+            menuBtn.classList.add('open');
+            navMenu.classList.add('active')
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open');
+            navMenu.classList.remove('active')
+            menuOpen = false;
+        }
+    })
+}
 
 class TypeWriter {
     constructor(txtElement, words, wait = 3000) {
@@ -84,13 +59,17 @@ class TypeWriter {
     }
 }
 
-// Init on DOM Load
-document.addEventListener('DOMContentLoaded', init);
-// Init App
-function init() {
+myApp.typingText = function () {
     const txtElement = document.querySelector('.text-type');
     const words = JSON.parse(txtElement.getAttribute('data-words'));
     const wait = txtElement.getAttribute('data-wait');
-    // Init TypeWriter
     new TypeWriter(txtElement, words, wait);
 }
+
+// Init App
+myApp.init = function () {
+    myApp.typingText();
+    myApp.toggleMenu();
+}
+
+myApp.init();
